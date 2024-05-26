b52b093ec4b9dc07452832c2f23d618ba77aaf14
Exctro <terrar222@gmail.com>
2024-05-26 13:49:09 +0200

    Added Simulation class, main is now in Simulation

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        Simulation a = new Simulation();
-        a.TestPrint();
-    }
-}
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,9 +1,16 @@
 public class Simulation {
 
-    Simulation() {
+    int time = 0;
+    int timeLimit;
 
-    }
     public void TestPrint() {
         System.out.println("test1");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Simulation a = new Simulation();
+        a.TestPrint();
     }
 }
