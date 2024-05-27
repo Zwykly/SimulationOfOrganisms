44a19f3b94acdfe35416192053507ba547025c92
Zwykly <craftmor12@gmail.com>
2024-05-27 18:09:54 +0200

    Fixed minor spelling error

--- src/MapSimple.java
+++ src/MapSimple.java
@@ -46,17 +46,17 @@ public class MapSimple implements IMap {
     @Override
     public void PrintMap(IMap map)
     {
-        char[][] visableMap = new char[size][size];
+        char[][] visibleMap = new char[size][size];
         for(int i = 0; i<size;i++)
         {
             for(int j = 0; j<size;j++) {
-                visableMap[i][j] = '#';
+                visibleMap[i][j] = ' ';
             }
         }
-        creaturesPositions.forEach((k,v) -> {if(k instanceof Carnivore){visableMap[v[0]][v[1]]='C';}});
+        creaturesPositions.forEach((k,v) -> {if(k instanceof Carnivore){visibleMap[v[0]][v[1]]='C';}});
         for(int i = 0; i<size;i++)
         {
-            System.out.println(visableMap[i]);
+            System.out.println(visibleMap[i]);
         }
     }
 }
