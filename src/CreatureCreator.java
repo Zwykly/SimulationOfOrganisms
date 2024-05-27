19b4bb3cf58fc1330584a8d0338575c213aff4b3
Zwykly <craftmor12@gmail.com>
2024-05-27 10:46:22 +0200

    Created CreatureCreator class but currently it only creates Carnivores.

--- src/CreatureCreator.java
+++ src/CreatureCreator.java
@@ -1,2 +1,30 @@
-public class CreatureCreator {
+import java.util.LinkedList;
+import java.util.List;
+
+public class CreatureCreator implements ICreatureCreator {
+    protected int numCarnivores;
+    protected int numOmnivores;
+    protected int numHerbivores;
+    public CreatureCreator()
+    {
+        this.numCarnivores = 5;
+        this.numOmnivores = 0;
+        this.numHerbivores = 0;
+    }
+    public CreatureCreator(int numCarnivores, int numOmnivores, int numHerbivores)
+    {
+        this.numCarnivores = numCarnivores;
+        this.numOmnivores = numOmnivores;
+        this.numHerbivores = numHerbivores;
+    }
+
+    @Override
+    public List<ICreature> CreateCreatures(IMap map) {
+        List<ICreature> creatureList = new LinkedList<>();
+        for(int i = 0; i<numCarnivores; i++)
+        {
+            creatureList.add(new Carnivore(map));
+        }
+        return creatureList;
+    }
 }
--- src/ICreatureCreator.java
+++ src/ICreatureCreator.java
@@ -1,2 +1,4 @@
+import java.util.List;
 public interface ICreatureCreator {
+    public List<ICreature> CreateCreatures(IMap map);
 }
