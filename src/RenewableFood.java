d8dc10159953b52cdb4041593f14194556bbe75b
Exctro <terrar222@gmail.com>
2024-06-07 20:24:40 +0200

    Added some functionality to RenewableFood

--- src/RenewableFood.java
+++ src/RenewableFood.java
@@ -3,9 +3,22 @@ public class RenewableFood extends Food
     public int regen_time = 2;
     public RenewableFood(IMap map) { super(map); }
 
+    // If regen_time is > 0 then the RenewableFood is still under regeneration
+    public boolean getRegenerationStatus(IObject object)
+    {
+        if(regen_time > 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void getEaten(IObject object)
     {
+        if(getRegenerationStatus(object))
+        {
 
+        }
     }
 }
